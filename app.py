43f50ccebc68b9c561010f7fd912fbf67c60402a
sathvik1996 <vsathvik1996@gmail.com>
2020-10-07 12:36:43 +0530

    Config.cfg added

--- app.py
+++ app.py
@@ -7,26 +7,33 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'b58f1ad3ab27a913c64246143682ebf5'
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'sathvik435teenz@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Vs@8106809666'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config.from_object(__name__)
+app.config.from_pyfile('config.cfg')
+
+
+# app.config['SECRET_KEY'] = 
+# app.config['MAIL_SERVER']='smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'sathvik435teenz@gmail.com'
+# app.config['MAIL_PASSWORD'] = 'Vs@8106809666'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
 
 mail = Mail(app)
 
 
-global User_data_retrived,Admin_data_retrived,User_data,Admin_data,db_mongo
+global user_data_retrived,admin_data_retrived,user_data,admin_data,db_mongo
 connect_link = "mongodb+srv://sathvik123:Systemtest123@cluster0.r6rot.mongodb.net/Indobytes-test?retryWrites=true&w=majority"
-cluster = MongoClient(connect_link)
+try:
+    cluster = MongoClient(connect_link)
+except:
+    print("Cannot Connect to Internet...")
 db_mongo= cluster["Indobytes-Test"]
-Admin_data = db_mongo['Indobytes-Test']
-Admin_data_retrived = Admin_data.find({})
+admin_data = db_mongo['Indobytes-Test']
+admin_data_retrived = admin_data.find({})
 
-User_data = db_mongo['Indobytes-Users']
-User_data_retrived = User_data.find({})
+user_data = db_mongo['Indobytes-Users']
+user_data_retrived = user_data.find({})
 
 global Admin_ID,Admin_password,fetch_data,send_mail
 
@@ -37,9 +44,12 @@ def send_mail(to_mail):
     msg = Message('IndoBytes SystemTest Demo', sender = 'sathvik435@gmail.com', recipients = [to_mail])
     OTP = randint(100000,999999)
     msg.body = "This is regarding confirmation of the Successfull Registration in the IndoBytes Website\n\n Your OTP is "+ str(OTP)
-    mail.send(msg)
-    print("Mail sent")
-    print("OTP = ",OTP)
+    try:
+        mail.send(msg)
+        print("Mail sent")
+        print("OTP = ",OTP)
+    except:
+        print("Unable to send email due to InValid Address")
     return "Sent"
     
     
@@ -48,8 +58,8 @@ def send_mail(to_mail):
     
 def fetch_data():
     global siid_arr,name_arr,email_arr,username_arr,password_arr,state_arr,index
-    User_data = db_mongo['Indobytes-Users']
-    User_data_retrived = User_data.find({})
+    user_data = db_mongo['Indobytes-Users']
+    user_data_retrived = user_data.find({})
     siid_arr = []
     name_arr = []
     email_arr = []
@@ -57,7 +67,7 @@ def fetch_data():
     password_arr = []
     state_arr = []
     index = 0
-    for user in User_data_retrived:
+    for user in user_data_retrived:
         siid_arr.append(user['siid'])
         name_arr.append(user['name'])
         email_arr.append(user['Email'])
@@ -73,22 +83,22 @@ def fetch_data():
 def Userprofile():
     
     if "user" in session:
-        Logged_User = session["user"]
-        Logged_user_name = session["username"]
+        logged_user = session["user"]
+        logged_user_name = session["username"]
     
         findmyquery_E = { "state": {"$regex":"Enable" } }
-        Enabled_count =  User_data.find(findmyquery_E).count()
+        Enabled_count =  user_data.find(findmyquery_E).count()
         print(Enabled_count)
 
         findmyquery_D = { "state": {"$regex":"Disable" } }
-        Disabled_count =  User_data.find(findmyquery_D).count()
+        Disabled_count =  user_data.find(findmyquery_D).count()
         print(Disabled_count)
 
         
-        Full_count = User_data.find().count()
+        Full_count = user_data.find().count()
         
         
-        return render_template("UserProfile.html",Logged_User = Logged_User,Logged_user_name=Logged_user_name,Enabled_count=Enabled_count,
+        return render_template("UserProfile.html",logged_user = logged_user,logged_user_name=logged_user_name,Enabled_count=Enabled_count,
                             Disabled_count=Disabled_count,Full_count=Full_count)
     return redirect(url_for('login'))
 
@@ -98,17 +108,17 @@ def login():
     if "user" in session:
         return redirect(url_for('Userprofile'))
         
-    global Logged_User,Logged_user_name
-    Logged_User = ""
-    Logged_user_name = ""
+    global logged_user,logged_user_name
+    logged_user = ""
+    logged_user_name = ""
     fetch_data()
     if request.method == 'POST':
         username = str(request.form['username'])
         password = str(request.form['password'])
         password_check_usr = False
         password_check_email = False
-        Personlogindata_usr = User_data.find_one({"username": username})
-        Personlogindata_email = User_data.find_one({"Email": username})
+        Personlogindata_usr = user_data.find_one({"username": username})
+        Personlogindata_email = user_data.find_one({"Email": username})
         
         if (Personlogindata_usr!=None):
             password_check_usr = check_password_hash(Personlogindata_usr["password"],password)
@@ -120,10 +130,10 @@ def login():
             if Personlogindata_usr["state"] == "Disable":
                 return render_template("login.html",status_login = "Disable")
             else:
-                Logged_User = Personlogindata_usr["username"] 
-                Logged_user_name =  Personlogindata_usr["name"]
-                session["user"] = Logged_User
-                session["username"] = Logged_user_name
+                logged_user = Personlogindata_usr["username"] 
+                logged_user_name =  Personlogindata_usr["name"]
+                session["user"] = logged_user
+                session["username"] = logged_user_name
                 return redirect(url_for('Userprofile'))
             
             
@@ -131,10 +141,10 @@ def login():
             if Personlogindata_email["state"] == "Disable":
                 return render_template("login.html",status_login = "Disable")
             else:
-                Logged_User = Personlogindata_email["username"] 
-                Logged_user_name =  Personlogindata_email["name"]
-                session["user"] = Logged_User
-                session["username"] = Logged_user_name
+                logged_user = Personlogindata_email["username"] 
+                logged_user_name =  Personlogindata_email["name"]
+                session["user"] = logged_user
+                session["username"] = logged_user_name
                 return redirect(url_for('Userprofile'))
       
       
@@ -153,7 +163,7 @@ def register():
     if request.method == 'POST':
         new_username = str(request.form['username'])
         new_username = new_username.replace("@","")
-        PersonREGdata_usr = User_data.find_one({"username": new_username})
+        PersonREGdata_usr = user_data.find_one({"username": new_username})
         if (PersonREGdata_usr!=None):
             return render_template("register.html",Register_login = "Exists")
         
@@ -162,14 +172,14 @@ def register():
         new_password = str(request.form['password'])
         
         
-        Full_count = User_data.find().count()
+        Full_count = user_data.find().count()
         Reg_Dict = { "siid":Full_count+1,
                     "name":new_name,
                     "Email":new_Email,
                     "username":new_username,
                     "password":generate_password_hash(new_password),
                     "state":"Enable"}
-        User_data.insert_one(Reg_Dict)
+        user_data.insert_one(Reg_Dict)
         send_mail(new_Email)
         return render_template("register.html",Register_login = "Success")
         
@@ -193,9 +203,9 @@ def adminpost():
 def admin():
     Admin_pass_check = False
     fetch_data()
-    Admin_data = db_mongo['Indobytes-Test']
-    Admin_data_retrived = Admin_data.find({})
-    for item in Admin_data_retrived:
+    admin_data = db_mongo['Indobytes-Test']
+    admin_data_retrived = admin_data.find({})
+    for item in admin_data_retrived:
         Admin_ID = item['adminID']
         Admin_password = item['password']
     print (Admin_password)
@@ -228,15 +238,15 @@ def forgotpassword():
         if password1 != password2 :
             return render_template("forgotpassword.html",status_login = "Error")
         else:
-            Personlogindata_usr = User_data.find_one({"username": username_person})
+            Personlogindata_usr = user_data.find_one({"username": username_person})
             if (Personlogindata_usr!=None ):
                 if Personlogindata_usr["state"] == "Disable":
                     return render_template("forgotpassword.html",status_login = "Disable")
                 else:
-                    User_data.update_one({"username":username_person},{"$set":{"password": generate_password_hash(password1)}})
+                    user_data.update_one({"username":username_person},{"$set":{"password": generate_password_hash(password1)}})
                     if "user" in session:
-                        Logged_user_name = session["username"]
-                        print(Logged_user_name)
+                        logged_user_name = session["username"]
+                        print(logged_user_name)
                     return render_template("forgotpassword.html",status_login = "Success")
                     
             else:
@@ -251,7 +261,7 @@ def forgotpassword():
 def delete(del_username):
     print(del_username)
     myquery = { "username": {"$regex":del_username } }
-    x = User_data.delete_many(myquery)
+    x = user_data.delete_many(myquery)
     print(x.deleted_count, " documents deleted.")
     return redirect(url_for('adminpost'))
 
@@ -259,7 +269,7 @@ def delete(del_username):
 def disable(dis_username,flag):
     print(dis_username)
     print(flag)
-    User_data.update_one({"username":dis_username},{"$set":{"state":flag }})
+    user_data.update_one({"username":dis_username},{"$set":{"state":flag }})
     return redirect(url_for('adminpost'))
 
 
@@ -267,7 +277,7 @@ def disable(dis_username,flag):
 @app.route('/Edit', methods=['GET', 'POST'])
 def Edit_post():
     if request.method == 'GET':
-        person_record = User_data.find_one({"username": edit_username_gl})
+        person_record = user_data.find_one({"username": edit_username_gl})
         print(person_record)
         edit_name = person_record["name"]
         edit_email = person_record["Email"]
@@ -280,7 +290,7 @@ def Edit_post():
         updtd_firstname = str(request.form['first_name'])
         updtd_email = str(request.form['email'])
         updtd_password = str(request.form['password'])
-        User_data.update_one({"username":edit_username_gl},{"$set":{"name":updtd_firstname,
+        user_data.update_one({"username":edit_username_gl},{"$set":{"name":updtd_firstname,
                                                                     "Email":updtd_email,
                                                                     "username":updtd_username,
                                                                     "password":generate_password_hash(updtd_password),
